ca43c694bbc9e648d39bad26ed0b19d3949685e5
Tanmay Trivedi <tanmaytrivedi57@gmail.com>
2024-05-22 21:52:07 +0530

    updated server.js

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const bodyparser = require("body-parser");
-const cors = require("cors");
+
 require("dotenv").config();
  const connectDb = require("./dbconfig/dbconfig");
 
-const port = process.env.port || 5000;
+const port = 5000;
 const dummy = require("./Routes/route");
- connectDb();
+
 const app = express();
 app.use(express.json());
 app.use(bodyparser.json());
-app.use(cors());
+
 
 // admin routes---------------------------
 
